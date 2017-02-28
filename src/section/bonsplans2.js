fe2e59616e378735f836a92cfd17ff1e5e5ae3d4
Adamska <adams.roy27@gmail.com>
2017-02-28 14:56:09 +0100

    test

---
+++ src/section/bonsplans2.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './bonsplans2.css';
+
+class Section extends Component {
+  render() {
+    return (
+      <div className="Section">
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
+
+export default Section;
