a475ec721ac042f0c432cc1b2de5a9c24766ac64
Adamska <adams.roy27@gmail.com>
2017-03-02 14:35:16 +0100

    changement Adams

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Section from './section/bonsplans2.js';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import Header from './header.js'
+import Header from './header.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <Header></Header>
         <Section></Section>
       </div>
