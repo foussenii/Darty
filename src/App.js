20776ded3d82a143c080b54392f2c1ce98b0244f
ghunbot dawit <ghunbotdawit@outlook.com>
2017-03-06 15:02:15 +0100

    gunbot

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './newsLetter.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import Header from './header.js'
+import NewsLetter from './newsLetter.js'
 
 class App extends Component {
   render() {
     return (
-      <Header></Header>
+      <div class="container">
+        <NewsLetter></NewsLetter>
+      </div>
     );
   }
 }
