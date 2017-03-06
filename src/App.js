ff2310aeae1c3c3e09b74ccb5d50ce2330cf9041
Sadia <sadia93380@gmail.com>
2017-03-06 14:08:55 +0100

    Sadia

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import Header from './header.js'
+import Sadia from './Sadia.js';
+
+
 
 class App extends Component {
   render() {
     return (
-      <Header></Header>
+      <div>
+        <Sadia></Sadia>
+      </div>
     );
   }
 }
